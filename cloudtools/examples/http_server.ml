fd8e0fb47b74fd6e3a2f93a080cf351ee1f6349f
Markus Rudy <webmaster@burgerdev.de>
2018-05-03 22:35:42 +0200

    improve examples
    - more narrative around what's happening
    - actual demo of init
    - use meaningful path in http example

--- cloudtools/examples/http_server.ml
+++ cloudtools/examples/http_server.ml
@@ -1,13 +1,18 @@
 open Cloudtools.Http
 open Lwt
 
+(* A simple HTTP handler that responds to every request with the canonical
+   greeting message. *)
 let hello_handler _conn _req _body =
   Cohttp_lwt_unix.Server.respond_string ~status:`OK ~body:"Hello world!\n" ()
 
+(* The main method starts an HTTP server on localhost:8080 that greets the world
+   when accessed as [http://localhost:8080/hello]. It responds with an errpr
+   404 to all requests for a different path. *)
 let _ =
   Lwt_main.run
   @@ Server.serve ("localhost", 8080)
   @@ Proxy.reverse_proxy_opt
-  @@ fun r -> match Cohttp.Request.uri r |> Uri.path with
-  | "/echo" -> Some hello_handler
+  @@ fun request -> match Cohttp.Request.uri request |> Uri.path with
+  | "/hello" -> Some hello_handler
   | _ -> None
--- cloudtools/examples/pid1.ml
+++ cloudtools/examples/pid1.ml
@@ -1,9 +1,15 @@
 open Cloudtools.Docker
 
+(* This example runs under supervision of [init]. It forks off an additional
+   process that just falls asleep for a period of time and kills itself with a
+   SIGTERM. Try running this example without [init] - the sleeping child process
+   will not be signalled, thus stick around with new parent PID 1. Wrapped with
+   [init], the process won't exit until all children are gone, and if you signal
+   the process (by hitting Ctrl-C), child and parent will terminate as expected. *)
 let _ = init @@ fun _ ->
-  Printf.printf "Hello, fork\n";
-  flush_all ();
   match Unix.fork () with
-  | 0 ->
-    Unix.sleep 1000; exit 0
-  | _ -> Unix.sleep 1000; exit 0
+  | 0 -> Unix.sleep 1000; exit 0
+  | _ ->
+    Printf.printf "Committing suicide.\n";
+    flush_all ();
+    Unix.kill (Unix.getpid ()) Sys.sigterm; exit 0
